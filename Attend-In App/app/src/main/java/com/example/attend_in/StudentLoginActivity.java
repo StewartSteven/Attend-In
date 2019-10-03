58b37cd0875001e54974557ff91c420a7f2d7b5c
Steven Stewart <stewart.steven98@outlook.com>
2019-10-03 12:59:37 -0400

    continuing single sign in

--- Attend-In App/app/src/main/java/com/example/attend_in/StudentLoginActivity.java
+++ Attend-In App/app/src/main/java/com/example/attend_in/StudentLoginActivity.java
@@ -2,8 +2,10 @@ package com.example.attend_in;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,17 +13,31 @@ public class StudentLoginActivity extends AppCompatActivity {
     EditText usernameText, passwordText;
     Button login;
     private static final String prefs_file = "LoginInfo";
-    private static final String userName = "uname";
-    private static final String password = "pword";
+    private static final String userName = "Username";
+    private static final String password = "Password";
     SharedPreferences loginPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
-        usernameText = (EditText)findViewById(R.id.professorUserName);
-        passwordText = (EditText)findViewById(R.id.professorPassword);
-        login = (Button)findViewById(R.id.loginbutton);
+        usernameText = (EditText)findViewById(R.id.studentUserName);
+        passwordText = (EditText)findViewById(R.id.studentPassword);
+        login = (Button)findViewById(R.id.studentloginbutton);
+        loginPrefs = getSharedPreferences(prefs_file, Context.MODE_PRIVATE);
+
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String user = usernameText.getText().toString();
+                String pass = passwordText.getText().toString();
+                SharedPreferences.Editor editor = loginPrefs.edit();
+                editor.putString(userName, user);
+                editor.putString(password, pass);
+                editor.commit();
+            }
+        });
+
 
     }
 }
