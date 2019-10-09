d0f470beef61935c17ec6d60897224796dafd8ad
Steven Stewart <stewart.steven98@outlook.com>
2019-10-09 17:21:29 -0400

    Started working on md5 hash

--- Attend-In App/app/src/main/java/com/example/attend_in/ProfessorLoginActivity.java
+++ Attend-In App/app/src/main/java/com/example/attend_in/ProfessorLoginActivity.java
@@ -8,19 +8,11 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class ProfessorLoginActivity extends AppCompatActivity {
-    EditText usernameText, passwordText;
-    Button login;
-    private static final String prefs_file = "LoginInfo";
-    private static final String userName = "uname";
-    private static final String password = "pword";
-    SharedPreferences loginPrefs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        usernameText = (EditText)findViewById(R.id.professorUserName);
-        passwordText = (EditText)findViewById(R.id.professorPassword);
-        login = (Button)findViewById(R.id.loginbutton);
         setContentView(R.layout.activity_professor_login);
     }
 }
--- Attend-In App/app/src/main/java/com/example/attend_in/StudentLoginActivity.java
+++ Attend-In App/app/src/main/java/com/example/attend_in/StudentLoginActivity.java
@@ -1,7 +1,5 @@
 package com.example.attend_in;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -10,38 +8,47 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class StudentLoginActivity extends AppCompatActivity {
     EditText usernameText, passwordText;
     Button login;
-    private static final String prefs_file = "LoginInfo";
-    private static final String userName = "Username";
-    private static final String password = "Password";
-    SharedPreferences loginPrefs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
-        usernameText = (EditText)findViewById(R.id.studentUserName);
-        passwordText = (EditText)findViewById(R.id.studentPassword);
-        login = (Button)findViewById(R.id.studentloginbutton);
-        loginPrefs = getSharedPreferences(prefs_file, Context.MODE_PRIVATE);
 
-        login.setOnClickListener(new View.OnClickListener(){
+        login = (Button) findViewById(R.id.studentloginbutton);
+
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 String user = usernameText.getText().toString();
                 String pass = passwordText.getText().toString();
-                SharedPreferences.Editor editor = loginPrefs.edit();
-                editor.putString(userName, user);
-                editor.putString(password, pass);
-                editor.commit();
-                String u = loginPrefs.getString("Username", null).toString();
-                String p = loginPrefs.getString("Password", null).toString();
-                Toast.makeText(getApplicationContext(), u +" "+p, Toast.LENGTH_LONG).show();
+
             }
         });
 
 
     }
+
+    protected static String generateMD5(String uname, String pword) {
+        String hash = "";
+        MessageDigest md = null;
+        try{
+            md = MessageDigest.getInstance("MD5");
+        } catch(NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+
+        return hash;
+    }
+
+
 }
+
