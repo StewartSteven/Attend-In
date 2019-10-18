eafb6cd34cb93303b5c47111bcc687bc069ffd52
Steven Stewart <stewart.steven98@outlook.com>
2019-10-18 14:03:38 -0400

    Update StudentLoginActivity.java

--- Attend-In App/app/src/main/java/com/example/attend_in/StudentLoginActivity.java
+++ Attend-In App/app/src/main/java/com/example/attend_in/StudentLoginActivity.java
@@ -1,46 +1,27 @@
 package com.example.attend_in;
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.SharedPreferences;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import static java.lang.System.out;
 
 public class StudentLoginActivity extends AppCompatActivity {
     EditText usernameText, passwordText;
